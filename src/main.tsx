2bf9f3b4181c37b5c360d850b145864386725d8c
Nia Media <kariukinjoroge13@gmail.com>
2026-07-04 22:31:33 +0300

    Fix Cal timezone initialization

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,13 @@
-﻿import { StrictMode } from 'react'
+import { StrictMode } from 'react'
+import dayjs from 'dayjs'
+import utc from 'dayjs/plugin/utc'
+import timezone from 'dayjs/plugin/timezone'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+
+dayjs.extend(utc)
+dayjs.extend(timezone)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -14,4 +20,6 @@ if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/sw.js').catch(() => { /* non-fatal */ })
   })
 }
+
+
 
